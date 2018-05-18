11fd112f1e54cc43990f1978fc0bbe10931c52e2
Gowri Nandury <gowrikvk@gmail.com>
2018-05-18 15:25:28 +0530

    Update feature_detection.py

--- feature_detection.py
+++ feature_detection.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-
+import imutils
 class FeatureImageDetector:
 
     def __init__(imagedata):
@@ -26,11 +26,11 @@ class FeatureImageDetector:
     def registerFeature(imagedata):
         temp = True
         if temp == True:
+            #frame = cv2.imread('dollar.png')
             frame = cv2.imread('doll.jpg')
-            cv2.rectangle(frame, imagedata.rect_tl_outer_xy, imagedata.rect_br_outer_xy,\
-                          imagedata.rect_color, imagedata.rect_thickness)
-            cv2.imshow(" feature Image learning : ", frame)
-            cv2.waitKey(0)
+            frame = imutils.resize(frame)
+            # cv2.rectangle(frame, imagedata.rect_tl_outer_xy, imagedata.rect_br_outer_xy,\
+            #               imagedata.rect_color, imagedata.rect_thickness)
             imagedata.kp0, imagedata.des0 = imagedata.feature_detector.detectAndCompute(frame, None)
             imagedata.querying = frame
             imagedata.registered = True
@@ -40,28 +40,29 @@ class FeatureImageDetector:
         temp = True
         frame = cv2.imread('box_in_scene.jpg')
         frame = cv2.imread('chinchq.jpg')
+        frame = imutils.resize(frame)
         bkp_frame = cv2.imread('chinchq.jpg')
         # Keypoint (kp) detection and calculate descriptors (des)
         kp, des = imagedata.feature_detector.detectAndCompute(frame, None)
         winframe=imutils.resize(frame,1000,1000)
-        cv2.imshow(" Actual Img", winframe)        
+        cv2.imshow(" Actual Img", winframe)
         cv2.waitKey(0)
-        cv2.imshow(" Feature Image", imagedata.querying)
-        cv2.waitKey(0)
-        cv2.imshow(" no .... ", imagedata.querying)
+        cv2.imshow(" Feature Image : ", imagedata.querying)
         cv2.waitKey(0)
         # Apply blute-force knn matching between keypoints
         matches = bf.knnMatch(imagedata.des0, des, k=2)
         # Adopt only good feature matches
         goodfeatures = [[m] for m, n in matches if m.distance < imagedata.ratio * n.distance]
-
-        # Find Homography
+        print(len(goodfeatures))
+        #Find Homography
         if len(goodfeatures) > imagedata.min_match_count:
             src_pts = np.float32([imagedata.kp0[m[0].queryIdx].pt for m in goodfeatures]).reshape(-1, 1, 2)
             dst_pts = np.float32([kp[m[0].trainIdx].pt for m in goodfeatures]).reshape(-1, 1, 2)
             M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
             h, w, _ = imagedata.querying.shape  # Assume color camera
             pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
+            print(type(M))
+            print(len(M))
             dst = cv2.perspectiveTransform(pts, M)
 
             for i, coords in enumerate(dst):
@@ -75,7 +76,7 @@ class FeatureImageDetector:
                     x4, y4 = coords[0]
             cropped_img = bkp_frame[int(y1):int(y3), int(x2):]
             frame = cv2.polylines(frame, [np.int32(dst)], True, (0, 255, 0), 2, cv2.LINE_AA)
-        # Visualize the matches
+        #Visualize the matches
         draw_params = dict(matchColor=(0, 255, 0), singlePointColor=(0, 0, 255), flags=0)
         img = cv2.drawMatchesKnn(imagedata.querying, imagedata.kp0, frame, kp, goodfeatures, None, **draw_params)
         img=imutils.resize(img, 1000,1000)
